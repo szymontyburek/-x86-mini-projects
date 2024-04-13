8fcc63e662cdb417303622200c6cb9897f2319ec
Szymon Tyburek <szymontyburek7@gmail.com>
2024-04-13 10:31:43 -0700

    created a new project to experiment with floating point numbers

---
+++ Floating Point Numbers/data.s
@@ -0,0 +1,12 @@
+section .data
+        x dd 3.14
+        y dd 2.1
+section .text
+global _start
+
+_start:
+        movss xmm0,[x]
+        movss xmm1,[y]
+        addss xmm0,xmm1
+        int 80h
+        
